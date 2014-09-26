a575a58d0d1467901927240e3f9011a4747c1bcb
warsjava <warsjava@debian.warsjava>
2014-09-26 13:11:49 -0400

    controller stub3

---
+++ src/main/groovy/com/ofg/warsjawa/controller/Client.groovy
@@ -0,0 +1,13 @@
+package com.ofg.warsjawa.controller
+
+/**
+ * Created by warsjava on 9/26/14.
+ */
+class Client {
+    String name
+    String surname
+    Integer age
+    String jobPosition
+    Integer loanAmount
+
+}
--- src/main/groovy/com/ofg/warsjawa/controller/ClientController.groovy
+++ src/main/groovy/com/ofg/warsjawa/controller/ClientController.groovy
@@ -1,11 +1,13 @@
 package com.ofg.warsjawa.controller
 
+import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
 import com.ofg.twitter.controller.place.extractor.CityFinder
 import com.wordnik.swagger.annotations.Api
 import com.wordnik.swagger.annotations.ApiOperation
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,15 +21,17 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST
 @Api(value = "client", description = "Operations on clients")
 class ClientController {
 
-
+    @Autowired
+    def  ServiceRestClient serviceRestClient
 
     @RequestMapping(
             value = '/{name}/{surname}/{age}/{jobPosition}/{loanAmount}',
             method = POST)
     @ApiOperation(value = "proccess loan ",
             notes = "a mega api call")
-    String findCity( @NotNull String name, @NotNull String surname,@NotNull Integer age,@NotNull String jobPosition,Integer loanAmount) {
-        log.info("name: ${name}, surname: ${surname} age: ${age}, job:${jobPosition}, loanAmount: ${loanAmount}")
+    String applyForLoan(  @RequestBody @NotNull Client client) {
+        log.info("name: ${client.name}, surname: ${client.surname} age: ${client.age}, job:${client.jobPosition}, loanAmount: ${client.loanAmount}")
+        serviceRestClient.forService("")
         return "{}"
     }
 }
